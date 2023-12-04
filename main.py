ed58f332bd51ea8fffc4924138d0d7667d0112a1
79800 <art.kirichenko21@mail.ru>
2023-12-04 20:03:08 +0300

    itog

--- main.py
+++ main.py
@@ -5,24 +5,31 @@ from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
-#*[randint(0, 255) for _ in range(3)]
+
+# *[randint(0, 255) for _ in range(3)]
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.do_paint = False
         uic.loadUi('UI.ui', self)
-        self.drawB.clicked.connect(self.draw)
+        self.drawB.clicked.connect(self.drawf)
+
+    def drawf(self):
+        self.do_paint = True
+        self.update()
 
     def draw(self):
         R = randint(20, 100)
-        self.qp.setBrush(QColor(256, 256))
-        self.qp.drawEllipse((150, 150), R, R)
+        self.qp.setBrush(QColor(255, 255, 0))
+        self.qp.drawEllipse(150, 150, R, R)
 
     def paintEvent(self, event):
-        if self.flag:
+        if self.do_paint:
             self.qp = QPainter()
             self.qp.begin(self)
             self.draw()
             self.qp.end()
+        self.do_paint = False
 
 
 if __name__ == '__main__':
