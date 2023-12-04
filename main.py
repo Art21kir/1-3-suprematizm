3d61d65b7b4df69482fa5a86f7810f6490c4f197
79800 <art.kirichenko21@mail.ru>
2023-12-04 18:56:10 +0300

    itog

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+from random import randint
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+#*[randint(0, 255) for _ in range(3)]
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui')
+        self.drawB.clicked.connect(self.draw)
+
+    def draw(self):
+        R = randint(20, 100)
+        self.qp.setBrush(QColor(256, 256))
+        self.qp.drawEllipse((150, 150), R, R)
+
+    def paintEvent(self, event):
+        if self.flag:
+            self.qp = QPainter()
+            self.qp.begin(self)
+            self.draw()
+            self.qp.end()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Window()
+    ex.show()
+    sys.exit(app.exec_())
