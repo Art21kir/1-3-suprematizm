068165f4f77570429da1c6179e688ebac82d8fea
79800 <art.kirichenko21@mail.ru>
2023-12-04 18:57:57 +0300

    itog

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui')
+        uic.loadUi('UI.ui', self)
         self.drawB.clicked.connect(self.draw)
 
     def draw(self):
